cf4d33d7873d8e3a5c4c5915ae6b399781e33026
YusifAlceferov <alcafarovyusif1122@gmail.com>
2024-04-03 19:21:22 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,19 @@
+//--------------1
+// let Mehsullar = [
+//     { Məhsul: "Kitab", Sayı: 10, Qiyməti: 25 },
+//     { Məhsul: "Telefon", Sayı: 5, Qiyməti: 500 },
+//     { Məhsul: "", Sayı: 3, Qiyməti: 30 },
+//     { Məhsul: "Kamera", Sayı: 8, Qiyməti: 200 },
+//     { Məhsul: "Monitor", Sayı: 2, Qiyməti: 150 }
+// ];
+// Mehsullar.sort((a, b) => b.Qiyməti - a.Qiyməti);
+// Mehsullar.forEach(mehsul => console.log(mehsul.Qiyməti));
+
+//----------------------2
+
+// let Numbers = [];
+// for (let i = 0; i < 5; i++) {
+//     randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+// }
+// let total = Numbers.reduce((x, y) => x + y, 0);
+// console.log(" təsadüfi ədədlərin cəmi:", total);
